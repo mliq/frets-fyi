79ce32b58caf4f9d7ab385c575d7b434fa2ad2e0
jamesshedden <james@jshedden.com>
2017-02-19 22:51:15 +0000

    Toggle label styling

--- src/components/notes-intervals-toggle/notes-intervals-toggle.js
+++ src/components/notes-intervals-toggle/notes-intervals-toggle.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import './notes-intervals-toggle.css';
 
 export default class NotesIntervalsToggle extends Component {
   render() {
     return (
       <div className='u-flex-direction-row'>
-        <label>
+        <label className={ classNames('notes-intervals-toggle__label', {
+          'is-active': this.props.value === 'notes',
+        }) }>
           <input type="radio"
+          className='u-display-none'
           name="toggle"
           id="notes"
           value="notes"
@@ -15,8 +20,11 @@ export default class NotesIntervalsToggle extends Component {
           Notes
         </label>
 
-        <label>
+        <label className={ classNames('notes-intervals-toggle__label', {
+          'is-active': this.props.value === 'intervals',
+        }) }>
           <input type="radio"
+          className='u-display-none'
           name="toggle"
           id="intervals"
           value="intervals"
