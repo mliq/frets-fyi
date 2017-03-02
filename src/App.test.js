c8339727091bef6180a199d6fbe82cf9263f7bb7
jamesshedden <james@jshedden.com>
2017-03-02 23:37:35 +0000

    Refactoring App test & updating snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -7,21 +7,20 @@ import toJson from 'enzyme-to-json';
 import { NOTES } from '../src/constants/notes';
 import { SCALES } from '../src/constants/scales';
 
+const component = mount(<App />);
+const noteSelectEl = component.find('#note-select');
+const scaleSelectEl = component.find('#scale-select');
+const notesToggleEl = component.find('#notes');
+const intervalsToggleEl = component.find('#intervals');
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
 
-  const component = mount(
-    <App />
-  );
-
-  const noteSelectEl = component.find('#note-select');
-  const scaleSelectEl = component.find('#scale-select');
-  const notesToggleEl = component.find('#notes');
-  const intervalsToggleEl = component.find('#intervals');
-
-  NOTES.forEach((note) => {
-    SCALES.forEach((scale) => {
+NOTES.forEach((note) => {
+  SCALES.forEach((scale) => {
+    it(`renders ${note.name} ${scale.name}`, () => {
       noteSelectEl.simulate('change', { target: { value: note.name }});
       scaleSelectEl.simulate('change', { target: { value: scale.name }});
       notesToggleEl.simulate('change', { target: { checked: true }});
